7797f1d8b16b74485f45866589383cdaedd1a914
akiyamajyunta <phononiwasi@gmail.com>
2025-02-14 13:45:48 +0900

    new

---
+++ src/constant.ts
@@ -0,0 +1,20 @@
+import { ref } from "vue";
+import { getId} from "./record";
+
+
+
+export const title = ref<string>("")
+export const text = ref<string>("")
+
+
+export const tag = ref<string>("")
+export const sentence = ref<string>("")
+
+
+export const  storeId = ref<string>("")
+
+export const id = ref<number>(0)
+//export const  values: number[] = []
+
+export const idRecord: number[] = getId() //[] //test //[] //StartCallID()//[]// StartCallID()//[] //dateIdArray
+
