9734a4a693786c355c6ee66cc7b1daa521e32946
akiyamajyunta <phononiwasi@gmail.com>
2025-03-05 17:55:38 +0900

    2023/03/05

--- src/components/commons/inputCard/inputTime.vue
+++ src/components/commons/inputCard/inputTime.vue
@@ -4,7 +4,6 @@
         v-if="task.timeDisplay">
         <v-text-field
             v-if="task.timeDisplay"
-  
             variant="outlined"
             width="60%"
             v-model="task.time"
@@ -12,12 +11,12 @@
             <v-menu
                 :close-on-content-click="false"
                 activator="parent"
-                   label="時間"
+                label="時間"
                 transition="scale-transition"
                 >
                 <v-time-picker
-                   format="24hr"
-                    v-model="task.time">
+                format="24hr"
+                v-model="task.time">
                 </v-time-picker>
             </v-menu>
         </v-text-field>
